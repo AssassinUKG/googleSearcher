ef4bc20a5ce5842aa39bbb8870da3f63f55274f9
ac1d <more4money@gmail.com>
2023-07-04 09:37:40 +0100

    Update gsearcher.py

--- gSearcher/gsearcher.py
+++ gSearcher/gsearcher.py
@@ -4,39 +4,37 @@ from googleapiclient.discovery import build
 import argparse
 
 my_api_key = "Your GOOGLE API KEY"
-my_cse_id = "YOU Custom Search Engine ID"
+my_cse_id = "YOUR Custom Search Engine ID"
 
-def google_search(search_term, api_key, cse_id, **kwargs):
+def google_search(search_term, api_key, cse_id, num_results, start_page=1, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    res = service.cse().list(q=search_term, cx=cse_id, num=num_results, start=start_page, **kwargs).execute()
     return res['items']
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--searchTerm", help="Enter a searchterm!")
+parser.add_argument("-s", "--searchTerm", help="Enter a search term!")
+parser.add_argument("-n", "--numResults", type=int, default=10, help="Enter the number of results to fetch (max 100)")
 args = parser.parse_args()
-
 
 if args.searchTerm is None:
     print("Enter a search term, exiting")
     exit()
 
 searchTerm = args.searchTerm
+numResults = min(args.numResults, 100)  # Ensure the number of results is capped at 100
 
-results = google_search(
-    searchTerm, my_api_key, my_cse_id, num=10)
+# Calculate the number of pages required based on the desired number of results
+num_pages = (numResults - 1) // 10 + 1
 
-#print(results)
-# find all 100 results from page 1 to page 10
+# Find all results from page 1 to the required number of pages
 allResults = []
-for startPage in range(1, 10):
-    allResults.append(google_search(
-        searchTerm, my_api_key, my_cse_id, num=10, start=startPage))
+for startPage in range(1, num_pages + 1):
+    allResults.extend(google_search(
+        searchTerm, my_api_key, my_cse_id, num_results=min(numResults, 10), start_page=startPage))
 
 if len(allResults) == 0:
     print("No Results found")
 else:
     print("Results found")        
     for result in allResults:
-        for r in result:
-            print(r['link'])
-            #pprint.pprint(result)
+        print(result['link'])

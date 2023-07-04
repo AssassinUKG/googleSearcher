814ac67a26162bd4c48a487b9edafaae6a330c65
ac1d <more4money@gmail.com>
2023-07-04 09:59:33 +0100

    Update gsearcher.py

--- gSearcher/gsearcher.py
+++ gSearcher/gsearcher.py
@@ -10,12 +10,17 @@ my_cse_id = "YOUR Custom Search Engine ID"
 
 def google_search(search_term, api_key, cse_id, num_results, start_page=1, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, num=num_results, start=start_page, **kwargs).execute()
-    return res['items']
+    try:
+        res = service.cse().list(q=search_term, cx=cse_id, num=num_results, start=start_page, **kwargs).execute()
+        return res['items']
+    except Exception as e:
+        print("An error occurred during the search:", str(e))
+        return []
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--searchTerm", help="Enter a search term!")
 parser.add_argument("-n", "--numResults", type=int, default=10, help="Enter the number of results to fetch (max 100)")
+parser.add_argument("-p", "--page", type=int, default=1, help="Enter the page number")
 args = parser.parse_args()
 
 if args.searchTerm is None:
@@ -24,19 +29,15 @@ if args.searchTerm is None:
 
 searchTerm = args.searchTerm
 numResults = min(args.numResults, 100)  # Ensure the number of results is capped at 100
+page = args.page
 
-# Calculate the number of pages required based on the desired number of results
-num_pages = (numResults - 1) // 10 + 1
+# Find results for the specified page
+results = google_search(
+    searchTerm, my_api_key, my_cse_id, num_results=min(numResults, 10), start_page=page)
 
-# Find all results from page 1 to the required number of pages
-allResults = []
-for startPage in range(1, num_pages + 1):
-    allResults.extend(google_search(
-        searchTerm, my_api_key, my_cse_id, num_results=min(numResults, 10), start_page=startPage))
-
-if len(allResults) == 0:
+if len(results) == 0:
     print("No Results found")
 else:
-    print("Results found")        
-    for result in allResults:
+    print("Results found")
+    for result in results:
         print(result['link'])

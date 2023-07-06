aa5fda195ca38bd269881ee727339b57784c7839
ac1d <more4money@gmail.com>
2023-07-07 00:56:41 +0100

    Update gsearcher.py

--- gSearcher/gsearcher.py
+++ gSearcher/gsearcher.py
@@ -4,9 +4,10 @@
 #!/usr/bin/env python3
 from googleapiclient.discovery import build
 import argparse
+import itertools
 
 my_api_key = "Your GOOGLE API KEY"
-my_cse_id = "YOUR Custom Search Engine ID"
+my_cse_id = ["YOUR Custom Search Engine ID"] # you can add multiple engines for more results, ie: ["cse1", "cse2", cse3"]
 
 def google_search(search_term, api_key, cse_id, num_results, start_page=1, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
@@ -32,9 +33,12 @@ searchTerm = args.searchTerm
 numResults = min(args.numResults, 100)  # Ensure the number of results is capped at 100
 page = args.page
 
+rotating_cse_ids = itertools.cycle(my_cse_id)
+current_cse_id = next(rotating_cse_ids)
+
 # Find results for the specified page
 results = google_search(
-    searchTerm, my_api_key, my_cse_id, num_results=min(numResults, 10), start_page=page)
+    searchTerm, my_api_key, current_cse_id, num_results=min(numResults, 10), start_page=page)
 
 if len(results) == 0:
     print("No Results found")

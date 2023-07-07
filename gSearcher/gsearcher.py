7a0027ad78482f69975ec60e4ec168027b327239
ac1d <more4money@gmail.com>
2023-07-07 03:24:49 +0100

    Update gsearcher.py

--- gSearcher/gsearcher.py
+++ gSearcher/gsearcher.py
@@ -11,37 +11,38 @@ my_cse_id = ["YOUR Custom Search Engine ID"] # you can add multiple engines for 
 
 def google_search(search_term, api_key, cse_id, num_results, start_page=1, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
-    try:
-        res = service.cse().list(q=search_term, cx=cse_id, num=num_results, start=start_page, **kwargs).execute()
-        return res['items']
-    except Exception as e:
-        print("An error occurred during the search:", str(e))
-        return []
+    res = service.cse().list(q=search_term, cx=cse_id, num=num_results, start=start_page, **kwargs).execute()
+    return res['items']
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--searchTerm", help="Enter a search term!")
 parser.add_argument("-n", "--numResults", type=int, default=10, help="Enter the number of results to fetch (max 100)")
-parser.add_argument("-p", "--page", type=int, default=1, help="Enter the page number (1-10)")
 args = parser.parse_args()
 
 if args.searchTerm is None:
-    print("Created by Richard Jones")
-    parser.print_help()  # Print the help message if no search term is provided
+    print("Enter a search term:\n ./gsearcher.py -s \"search term\", exiting")
     exit()
 
 searchTerm = args.searchTerm
 numResults = min(args.numResults, 100)  # Ensure the number of results is capped at 100
-page = args.page
-rotating_cse_ids = itertools.cycle(my_cse_id)
-current_cse_id = next(rotating_cse_ids)    
 
-page = page * 10 + 1
-# Find results for the specified page
-results = google_search(
-    searchTerm, my_api_key, current_cse_id, num_results=numResults, start_page=min(page, 100))
+# Calculate the number of pages required based on the desired number of results
+num_pages = (numResults - 1) // 10 + 1
 
-if len(results) == 0:
+# Find all results from page 1 to the required number of pages
+allResults = []
+for startPage in range(0, num_pages + 1):
+    if startPage >= 1:
+        startPage = startPage * 10 + 1
+    if startPage >= 100:
+        break
+    print("page: ", startPage)
+    allResults.extend(google_search(
+        searchTerm, my_api_key, my_cse_id, num_results=min(numResults, 10), start_page=startPage))
+
+if len(allResults) == 0:
     print("No Results found")
 else:
-    for result in results:
+            
+    for result in allResults:
         print(result['link'])

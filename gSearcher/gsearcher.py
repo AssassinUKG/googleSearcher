40852b40c13a370392902a8cd3446e37b2c43aca
ac1d <more4money@gmail.com>
2023-07-07 01:13:03 +0100

    Update gsearcher.py
    Code improvement

--- gSearcher/gsearcher.py
+++ gSearcher/gsearcher.py
@@ -13,6 +13,9 @@ def google_search(search_term, api_key, cse_id, num_results, start_page=1, **kwa
     service = build("customsearch", "v1", developerKey=api_key)
     try:
         res = service.cse().list(q=search_term, cx=cse_id, num=num_results, start=start_page, **kwargs).execute()
+        totalResults = res['searchInformation']['totalResults']
+        if totalResults == '0':
+            return None
         return res['items']
     except Exception as e:
         print("An error occurred during the search:", str(e))
@@ -39,7 +42,9 @@ current_cse_id = next(rotating_cse_ids)
 # Find results for the specified page
 results = google_search(
     searchTerm, my_api_key, current_cse_id, num_results=min(numResults, 10), start_page=page)
-
+if not results:
+    print("No Results found")
+    exit()
 if len(results) == 0:
     print("No Results found")
 else:

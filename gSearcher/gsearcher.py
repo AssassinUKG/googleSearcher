df5e0cf4f2e0d0d6f36f60e79b9ab2d4237a9ac9
ac1d <more4money@gmail.com>
2023-07-07 03:30:56 +0100

    Update gsearcher.py

--- gSearcher/gsearcher.py
+++ gSearcher/gsearcher.py
@@ -17,32 +17,38 @@ def google_search(search_term, api_key, cse_id, num_results, start_page=1, **kwa
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--searchTerm", help="Enter a search term!")
 parser.add_argument("-n", "--numResults", type=int, default=10, help="Enter the number of results to fetch (max 100)")
+parser.add_argument("-p", "--page", type=int, default=1, help="Enter the page number")
 args = parser.parse_args()
 
 if args.searchTerm is None:
-    print("Enter a search term:\n ./gsearcher.py -s \"search term\", exiting")
+    print("Developed by: Richard Jones")
+    parser.print_help()
     exit()
 
 searchTerm = args.searchTerm
 numResults = min(args.numResults, 100)  # Ensure the number of results is capped at 100
 
-# Calculate the number of pages required based on the desired number of results
-num_pages = (numResults - 1) // 10 + 1
-
-# Find all results from page 1 to the required number of pages
 allResults = []
-for startPage in range(0, num_pages + 1):
-    if startPage >= 1:
-        startPage = startPage * 10 + 1
-    if startPage >= 100:
-        break
-    print("page: ", startPage)
+if args.page:
+    page = args.page - 1
     allResults.extend(google_search(
-        searchTerm, my_api_key, my_cse_id, num_results=min(numResults, 10), start_page=startPage))
+            searchTerm, my_api_key, my_cse_id, num_results=min(numResults, 10), start_page=page * 10 + 1 ))
+else:
+    # Calculate the number of pages required based on the desired number of results
+    num_pages = (numResults - 1) // 10 + 1
+    # Find all results from page 1 to the required number of pages
+   
+    for startPage in range(0, num_pages):
+        if startPage >= 1:
+            startPage = startPage * 10 + 1
+        if startPage >= 100:
+            break
+        
+        allResults.extend(google_search(
+            searchTerm, my_api_key, my_cse_id, num_results=min(numResults, 10), start_page=startPage))
 
 if len(allResults) == 0:
     print("No Results found")
-else:
-            
+else:            
     for result in allResults:
         print(result['link'])
